ccab137602dd5912f151aced06418f135ec78754
Nargis <125502580+ayyobi@users.noreply.github.com>
2025-11-07 08:40:27 +0100

    tp1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 // TODO
+// 1
+const paragraphe = document.querySelector("#a-mettre-en-rouge");
+paragraphe.style.color = "red";
+
+// 2
+const bouton = document.querySelector("#en-rouge-suite-a-click");
+bouton.addEventListener("click", () => {
+  bouton.style.color = "red";
+});
+
+// 3 
+const tousLesH2 = document.querySelectorAll("h2");
+tousLesH2.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    elem.style.color = "red";
+  });
+});
+
+
